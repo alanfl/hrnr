498a9aa97dfadfba659a87a852692469c7432236
alanfl <alanfl81199@gmail.com>
2021-03-26 20:16:00 -0400

    testing tool

---
+++ test/xrerun.sh
@@ -0,0 +1,2 @@
+./resume /replay_logdb/rec_$1 --pthread $OMNIPLAY_DIR/eglibc-2.15/prefix/lib
+dmesg
---
+++ test/xreset.sh
@@ -0,0 +1,4 @@
+rm -rf $OMNIPLAY_DIR/test/*.txt
+rm -rf /replay_logdb/*
+rm -rf /replay_cache/*
+$OMNIPLAY_DIR/test/reset_ndx
