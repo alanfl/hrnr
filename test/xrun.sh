9991e13f680876126c220d5645f732b30b0edded
alanfl <alanfl@users.noreply.github.com>
2021-03-28 03:12:36 -0400

    Fix xrun.sh to support xproxy

--- test/xrun.sh
+++ test/xrun.sh
@@ -1,2 +1,1 @@
-DISPLAY=:8.0
-$OMNIPLAY_DIR/test/launcher -m --pthread $OMNIPLAY_DIR/eglibc-2.15/prefix/lib:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/usr/lib:/lib $1
+DISPLAY=:8.0 $OMNIPLAY_DIR/test/launcher -m --pthread $OMNIPLAY_DIR/eglibc-2.15/prefix/lib:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/usr/lib:/lib $1
