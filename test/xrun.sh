6baef7149c045c622eddec02e6b42952a8c092d0
alanfl <alanfl@users.noreply.github.com>
2021-03-27 23:25:52 -0400

    Fix incidental new line

--- test/xrun.sh
+++ test/xrun.sh
@@ -1,2 +1,1 @@
-$OMNIPLAY_DIR/test/launcher -m --pthread $OMNIPLAY_DIR/eglibc-2.15/prefix/lib:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/usr/l
-ib:/lib $1
+$OMNIPLAY_DIR/test/launcher -m --pthread $OMNIPLAY_DIR/eglibc-2.15/prefix/lib:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/usr/lib:/lib $1
