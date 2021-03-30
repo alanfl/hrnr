e19c91c9a1a2d7e435899a8f66c6d9466ae2a1a3
alanfl <alanfl81199@gmail.com>
2021-03-29 20:16:08 -0400

    Better cleanup

--- test/xreset.sh
+++ test/xreset.sh
@@ -1,4 +1,8 @@
 rm -rf $OMNIPLAY_DIR/test/*.txt
 rm -rf /replay_logdb/*
 rm -rf /replay_cache/*
+rm -rf /tmp/[0-9]*
+rm -rf /tmp/query_log
+rm -rf /tmp/interpret_result
+rm -rf /tmp/output.dot
 $OMNIPLAY_DIR/test/reset_ndx
